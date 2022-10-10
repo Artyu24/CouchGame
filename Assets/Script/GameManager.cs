76edffadf3c421b56fac7e9f156fb40d249d9c68
Tos927 <t.sautereau92@gmail.com>
2022-10-10 15:00:50 +0200

    Begin player system

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private Dictionary<int, Player> players = new Dictionary<int, Player>();
+
+    void Awake()
+    {   
+        if (instance == null)
+            instance = this;
+    }
+
+    public void AddPlayer()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("LostPlayer");
+        Player dataPlayer = player.GetComponent<Player>();
+        players.Add(players.Count + 1, dataPlayer);
+        dataPlayer.playerID = players.Count;
+        if (dataPlayer.playerID == 1)
+        {
+            //player.transform.position = spawnJ1.position;
+            //player.name = PseudoManager.pseudoJ1;
+            //boxTutoJ1Text.SetActive(false);
+            //boxTutoJ2Text.SetActive(true);
+        }
+        else
+        {
+            //player.transform.position = spawnJ2.position;
+            //player.name = PseudoManager.pseudoJ2;
+            //boxTutoJ2Text.SetActive(false);
+        }
+        player.tag = "Player";
+    }
+}
