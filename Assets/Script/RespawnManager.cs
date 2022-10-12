9e32ed52f5900fb5b2e3ef1eb05474fb14bc9bad
DrTheDraGNaK <jean-michel.giverne@edu.devinci.fr>
2022-10-12 11:19:19 +0200

    playerSpawnDone

--- Assets/Script/RespawnManager.cs
+++ Assets/Script/RespawnManager.cs
@@ -1,84 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RespawnManager : MonoBehaviour
-{
-    
-   public GameObject player;
-   [Header("-------Mettre Tout les spawner-------")]
-   [SerializeField]
-   Transform[] respawn;
-
-   public float timeToRespawn;
-   
-
-   void Start()
-   {
-       //respawn = new Transform[transform.childCount];
-
-       //for (int i = 0; i < transform.childCount; i++)
-       //{
-       //    respawn[i] = transform.GetChild(i);
-       //}
-   }
+{   
+       
+ 
+   public float timeToRespawn;   
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
-        {
-            StartCoroutine(Die());
-
-        }
-        
-    }
-
-    public IEnumerator Die()
-    {
-        //yield return new WaitForSeconds(timeToRespawn);
-        //int ran = Random.Range(0, transform.childCount);
-        //player.transform.position = respawn[ran].transform.position;
-
-
-
-        int ran = Random.Range(0, 8);
-
-        switch (ran)
-        {
-            case 0:
-
-                yield return new WaitForSeconds(timeToRespawn);
-                player.transform.position = respawn[0].position;
-                break;
-            case 1:
-
-                yield return new WaitForSeconds(timeToRespawn);
-                player.transform.position = respawn[1].position;
-                break;
-            case 2:
-                yield return new WaitForSeconds(timeToRespawn);
-                player.transform.position = respawn[2].position;
-                break;
-            case 3:
-                yield return new WaitForSeconds(timeToRespawn);
-                player.transform.position = respawn[3].position;
-                break;
-            case 4:
-                yield return new WaitForSeconds(timeToRespawn);
-                player.transform.position = respawn[4].position;
-                break;
-            case 5:
-                yield return new WaitForSeconds(timeToRespawn);
-                player.transform.position = respawn[5].position;
-                break;
-            case 6:
-                yield return new WaitForSeconds(timeToRespawn);
-                player.transform.position = respawn[6].position;
-                break;
-            case 7:
-                yield return new WaitForSeconds(timeToRespawn);
-                player.transform.position = respawn[7].position;
-                break;
-        }
-        yield break;
+        {          
+            StartCoroutine(other.gameObject.GetComponent<PlayerRespawn>().Die());
+        }      
     }
 }
