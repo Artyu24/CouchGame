bcf88b6d82dbd76eae808f348b5dbe0a8948e4de
Antirion <twitoine@gmail.com>
2022-10-12 16:06:10 +0200

    [Adding PointArea + Juiciness] - Fix Merge issue

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -17,71 +17,45 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float deadZone = 0.3f;
 
     [SerializeField] private PlayerInput playerInput;
-<<<<<<< Updated upstream
     private Player player;
-    
+
     private int actualCircle;
     private float rotation = 0;
     [SerializeField] private float circleSpeed = 5;
     public Color activeColor;
     public Color beginColor;
     private MeshRenderer[] childrenMeshRenderers;
-=======
     public Animator animator;
->>>>>>> Stashed changes
 
     // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
-<<<<<<< Updated upstream
         player = GetComponent<Player>();
-        beginColor = GameManager.instance.TabCicle[actualCircle].GetComponentInChildren<MeshRenderer>().material.color;
-=======
+        //beginColor = GameManager.instance.TabCicle[actualCircle].GetComponentInChildren<MeshRenderer>().material.color;
         playerState = GetComponent<Player>().ActualPlayerState;
         animator = GetComponentInChildren<Animator>();
->>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
-        if (player.ActualPlayerState == PlayerState.MIDDLE)
-        {
-            //input system middle
-            childrenMeshRenderers = GameManager.instance.TabCicle[actualCircle].GetComponentsInChildren<MeshRenderer>();
-            foreach (var child in childrenMeshRenderers)
-            {
-                child.material.color = activeColor;
-            }
-            GameManager.instance.TabCicle[actualCircle].transform.eulerAngles = new Vector3(0,
-                GameManager.instance.TabCicle[actualCircle].transform.eulerAngles.y +
-                (rotation * speed * Time.fixedDeltaTime), 0);
-        }
-        else if (player.ActualPlayerState == PlayerState.FIGHTING)
-        {
-            //input system normal
-=======
+
         if (playerState != PlayerState.DEAD)
         {
->>>>>>> Stashed changes
             rb.MovePosition(rb.position + movementInput * Time.fixedDeltaTime * speed);
             transform.rotation = orientation;
         }
     }
-    
+
 
     public void OnMove(InputAction.CallbackContext ctx)
     {
-<<<<<<< Updated upstream
-        if (player.ActualPlayerState == PlayerState.FIGHTING)
+        animator.SetFloat("Magnitude", ctx.ReadValue<Vector3>().sqrMagnitude);
+        movementInput = ctx.ReadValue<Vector3>();
+        if (ctx.performed && ctx.ReadValue<Vector3>().sqrMagnitude > (deadZone * deadZone))
         {
-            movementInput = ctx.ReadValue<Vector3>();
-            if (ctx.performed)
-            {
-                orientation = quaternion.LookRotation(ctx.ReadValue<Vector3>(), Vector3.up);
-            }
+            orientation = quaternion.LookRotation(ctx.ReadValue<Vector3>(), Vector3.up);
         }
     }
 
@@ -104,7 +78,8 @@ public class PlayerMovement : MonoBehaviour
         {
             if (context.started)
             {
-                childrenMeshRenderers = GameManager.instance.TabCicle[actualCircle].GetComponentsInChildren<MeshRenderer>();
+                childrenMeshRenderers =
+                    GameManager.instance.TabCicle[actualCircle].GetComponentsInChildren<MeshRenderer>();
                 foreach (var child in childrenMeshRenderers)
                 {
                     child.material.color = beginColor;
@@ -118,19 +93,14 @@ public class PlayerMovement : MonoBehaviour
                 else
                     actualCircle += (int) nextCircle;
 
-                childrenMeshRenderers = GameManager.instance.TabCicle[actualCircle].GetComponentsInChildren<MeshRenderer>();
+                childrenMeshRenderers =
+                    GameManager.instance.TabCicle[actualCircle].GetComponentsInChildren<MeshRenderer>();
                 foreach (var child in childrenMeshRenderers)
                 {
                     child.material.color = activeColor;
                 }
             }
-=======
-        animator.SetFloat("Magnitude", ctx.ReadValue<Vector3>().sqrMagnitude);
-        movementInput = ctx.ReadValue<Vector3>();
-        if (ctx.performed && ctx.ReadValue<Vector3>().sqrMagnitude > (deadZone * deadZone))
-        {            
-            orientation = quaternion.LookRotation(ctx.ReadValue<Vector3>(), Vector3.up);
->>>>>>> Stashed changes
+
         }
     }
 }
