5dfbe66dab11621dd402be300eea5801b074f907
Tos927 <t.sautereau92@gmail.com>
2022-10-27 09:35:13 +0200

    Update color script

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -22,6 +22,7 @@ public class Timer : MonoBehaviour
     private Text[] scoreGeneralPlayerText = new Text[4];
     public GameObject roundScoreTextPrefab, generalScoreTextPrefab;
     public List<GameObject> medals = new List<GameObject>();
+    //private int nbrOfRound = 0;
     #endregion
 
     private void Awake()
@@ -102,19 +103,29 @@ public class Timer : MonoBehaviour
         {
             temp = Instantiate(generalScoreTextPrefab, textParentGeneral.transform);
             scoreGeneralPlayerText[p] = temp.GetComponentInChildren<Text>();
-
-            //temp.name = "Player " + tempPlayerListPlayer[p].playerID;
-            Debug.Log(tempPlayerListPlayer[p].playerID);
+            temp.name = "Player " + tempPlayerListPlayer[p].playerID;
             scoreGeneralPlayerText[p].text = "Player " + tempPlayerListPlayer[p].playerID + " : ";
 
             for (int i = 0; i < (tempPlayerListPlayer.Count-p+1); i++)
             {
-                InstantiateMedals(temp.transform, position);
-                GameManager.instance.players[p + 1].scoreGeneral++;
+                if (tempPlayerListPlayer[p].score > 0)
+                {
+                    InstantiateMedals(temp.transform, position);
+                    GameManager.instance.players[p + 1].scoreGeneral++;
+                }
+                /*if (nbrOfRound >= 1)
+                {
+                    for (int j = 0; j < GameManager.instance.players[p + 1].scoreGeneral; j++)
+                    {
+                        InstantiateMedals(temp.transform, position);
+                    }
+                }*/
             }
 
             position++;
         }
+
+        //nbrOfRound++;
     }
 
     private void InstantiateMedals(Transform t, int position)
@@ -123,10 +134,9 @@ public class Timer : MonoBehaviour
 
     }
 
-    private IEnumerator ReloadScene()
+    public void ReloadScene()
     {
-        Time.timeScale = 0;
-        SceneManager.LoadScene("Proto_Vincent");
-        yield return new WaitForSeconds(3);
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Proto_Téo");
     }
 }
