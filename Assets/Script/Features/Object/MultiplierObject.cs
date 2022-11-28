7059feb4c1d70f0fc11ea054e245c5657bcecf9f
Artyu <arthurpejoiane@hotmail.fr>
2022-11-28 16:55:17 +0100

    Add Interact Multiplayer Object

--- Assets/Script/Features/Object/MultiplierObject.cs
+++ Assets/Script/Features/Object/MultiplierObject.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class MultiplierObject : MonoBehaviour, IInteractable
 {
-    public void Interact()
+    public void Interact(Player player)
     {
-        
+        player.Multiplier = true;
+        StartCoroutine(ObjectManager.Instance.StopMultiplier(player));
     }
 }
