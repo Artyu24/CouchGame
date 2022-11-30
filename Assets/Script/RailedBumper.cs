18c205d7d90fdec85df35a7b532e18075b71b0ff
DrTheDraGNaK <jean-michel.giverne@edu.devinci.fr>
2022-11-30 15:48:30 +0100

    try something with bumper

--- Assets/Script/RailedBumper.cs
+++ Assets/Script/RailedBumper.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using static UnityEngine.GraphicsBuffer;
 
-public class RailedBumper : MonoBehaviour
+public class RailedBumper : MonoBehaviour, IInteractable
 {
     float timeCounter = 0;
 
@@ -13,6 +13,9 @@ public class RailedBumper : MonoBehaviour
     float speedHitBySuperStrenght;
     float position;
     public Transform igloo;
+
+    bool sensNormal = false;
+    bool sensInverse = false;
     private void Start()
     {
         speedAiguilleMontre = 2;
@@ -23,12 +26,53 @@ public class RailedBumper : MonoBehaviour
 
     private void Update()
     {
-        timeCounter += Time.deltaTime* speedAiguilleMontre;
+        if(sensNormal == true)
+        {
+            timeCounter += Time.deltaTime * speedAiguilleMontre;
 
-        float x = Mathf.Cos(timeCounter)* position;
-        float z = Mathf.Sin(timeCounter)* position;
+            float x = Mathf.Cos(timeCounter) * position;
+            float z = Mathf.Sin(timeCounter) * position;
 
-        transform.position = new Vector3(x, gameObject.transform.position.y, z);
-        transform.LookAt(igloo);
+            transform.position = new Vector3(x, gameObject.transform.position.y, z);
+            transform.LookAt(igloo);
+
+        }
+        if(sensInverse == true)
+        {
+            timeCounter += Time.deltaTime * speedInverseAiguilleMontre;
+
+            float x = Mathf.Cos(timeCounter) * position;
+            float z = Mathf.Sin(timeCounter) * position;
+
+            transform.position = new Vector3(x, gameObject.transform.position.y, z);
+            transform.LookAt(igloo);
+        }
+        
+    }
+
+    public void Interact(Player player = null)
+    {
+        if(player.transform.position.y - gameObject.transform.position.y < 0)
+        {
+            StartCoroutine(ForSensNoramal());
+        }
+        if (player.transform.position.y - gameObject.transform.position.y > 0)
+        {
+            StartCoroutine(ForSensInverse());
+        }
+
+    }
+
+    public IEnumerator ForSensNoramal()
+    {
+        sensNormal = true;
+        yield return new WaitForSeconds(2f);
+        sensNormal = false;
+    }
+    public IEnumerator ForSensInverse()
+    {
+        sensInverse = true;
+        yield return new WaitForSeconds(2f);
+        sensInverse = false;
     }
 }
