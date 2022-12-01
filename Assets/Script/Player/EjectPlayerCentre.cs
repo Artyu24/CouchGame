5e3b7e4104d561b0fd0b197f243aa2ed17ff1855
Antirion <twitoine@gmail.com>
2022-12-01 11:31:49 +0100

    [Integration of eject button]

--- Assets/Script/Player/EjectPlayerCentre.cs
+++ Assets/Script/Player/EjectPlayerCentre.cs
@@ -16,8 +16,10 @@ public class EjectPlayerCentre : MonoBehaviour
         {
             GameManager.instance.ejectPlatesActive++;
             bc.enabled = false;
-            GetComponentInChildren<MeshRenderer>().material.color = GameManager.instance.ActivatedColor;
+            //GetComponentInChildren<MeshRenderer>().material.color = GameManager.instance.ActivatedColor;
             ScoreManager.instance.AddScore(ScoreManager.instance.scoreInterrupteur, other.GetComponent<Player>());
+
+            GetComponent<Animator>().SetTrigger("Press");
 
             if (GameManager.instance.ejectPlatesActive >= GameManager.instance.NumberOfPlate)
             {
