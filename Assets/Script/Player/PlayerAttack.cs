6be2ed14d6b7d4f4056b1221b2e137324d1f3e9e
Artyu <arthurpejoiane@hotmail.fr>
2022-12-02 10:44:53 +0100

    Fix Glowing Effect when we have our unlimited power

--- Assets/Script/Player/PlayerAttack.cs
+++ Assets/Script/Player/PlayerAttack.cs
@@ -167,7 +167,7 @@ public class PlayerAttack : MonoBehaviour
         currentSpecial += _point;
         speBarreSlider.value = currentSpecial;
 
-        if(currentSpecial == maxSpecial)
+        if(currentSpecial >= maxSpecial)
             effectSpeBarre.SetActive(true);
     }
 
