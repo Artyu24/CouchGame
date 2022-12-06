256455fef30c1dd0ebf9f06832c5a465fce0379a
Tos927 <t.sautereau92@gmail.com>
2022-12-06 16:35:22 +0100

    Grosse update

--- Assets/Script/Manager/Timer.cs
+++ Assets/Script/Manager/Timer.cs
@@ -24,6 +24,7 @@ public class Timer : MonoBehaviour
 
     [SerializeField] private GameObject scoreWindowRound, scoreWindowGeneral;
     [SerializeField] private Sprite[] ppWindowRound = new Sprite[4];
+    [SerializeField] private Sprite[] backgroundWindowRound = new Sprite[4];
     [SerializeField] private GameObject textParentRound, textParentGeneral;
     public GameObject roundScoreTextPrefab, generalScoreTextPrefab; 
 
@@ -155,7 +156,7 @@ public class Timer : MonoBehaviour
     private void PrintScoreWindow()
     {
         scoreWindowRoundIsActive = true;
-        scoreWindowRound.GetComponent<RectTransform>().DOMoveY(500, 2);
+        scoreWindowRound.GetComponent<RectTransform>().DOAnchorPosY(0, 2);
         //Time.timeScale = 0;
         scoreWindowRound.SetActive(scoreWindowRoundIsActive);
 
@@ -176,13 +177,19 @@ public class Timer : MonoBehaviour
 
     public void PrintGeneralScoreWindow()
     {
-        scoreWindowRoundIsActive = false;
-        scoreWindowRound.SetActive(scoreWindowRoundIsActive);
+        Sequence mySequence2 = DOTween.Sequence();
+        mySequence2.Append(scoreWindowRound.GetComponent<RectTransform>().DOAnchorPosY(1000, 2));
+        mySequence2.onComplete += () =>
+        {
+            scoreWindowRoundIsActive = false;
+            scoreWindowRound.SetActive(scoreWindowRoundIsActive);
+        };
         
         if (!scoreWindowGeneralIsActive)
         {
             scoreWindowGeneralIsActive = true;
             scoreWindowGeneral.SetActive(scoreWindowGeneralIsActive);
+            scoreWindowGeneral.GetComponent<RectTransform>().DOAnchorPosY(0, 2);
             List<Player> tempPlayerListPlayer = new List<Player>();
             int position = 0;
 
@@ -209,6 +216,8 @@ public class Timer : MonoBehaviour
             {
                 temp = Instantiate(generalScoreTextPrefab, textParentGeneral.transform);
                 scoreGeneralPlayerText[p] = temp.GetComponentInChildren<Text>();
+                temp.GetComponent<Transform>().GetChild(0).GetComponent<Image>().sprite = ppWindowRound[p];
+                temp.GetComponent<Image>().sprite = backgroundWindowRound[p];
                 temp.name = "Player " + (tempPlayerListPlayer[p].playerID + 1);
                 scoreGeneralPlayerText[p].text = "Player " + (tempPlayerListPlayer[p].playerID + 1) + " : ";
 
