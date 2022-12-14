93808ce60b14d7635a2d13498debc35ed641c036
Jean-Michel <jean-michel.giverne@edu.devinci.fr>
2022-12-15 00:38:02 +0100

    player movement

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -64,7 +64,19 @@ public class PlayerMovement : MonoBehaviour
             }
 
             if (player.ActualPlayerState == PlayerState.MIDDLE)
+            {
                 GameManager.instance.tabCircle[actualCircle].transform.eulerAngles = new Vector3(0, GameManager.instance.tabCircle[actualCircle].transform.eulerAngles.y + (rotation * GameManager.instance.CircleRotationSpeed * Time.fixedDeltaTime), 0);
+                if (Math.Abs(rotation) > 0)
+                {
+                    FindObjectOfType<AudioManager>().PlayRandom(SoundState.RotateCircleSound);
+
+                }
+                else
+                {
+                    FindObjectOfType<AudioManager>().Stop(SoundState.RotateCircleSound);
+
+                }
+            }
         }
     }
 
@@ -136,7 +148,9 @@ public class PlayerMovement : MonoBehaviour
 
                         //son rota
 
+
                     }
+                    
                 }
             }
             else
